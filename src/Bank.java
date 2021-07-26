60d649a34b2d6dd95a4fe89fab2c460ca0556f9f
Xiaoyun Liao <sharon.liaoxy@gmail.com>
2021-07-26 11:27:57 -0400

    test

--- src/Bank.java
+++ src/Bank.java
@@ -5,6 +5,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank implements Runnable{
+//    test
     String name;
     int resource;
     int remainResouce;
